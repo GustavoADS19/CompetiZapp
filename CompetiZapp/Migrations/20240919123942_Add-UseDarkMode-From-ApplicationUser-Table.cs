24552ec10f3ac7210a288c31337a0c322039ab54
Gustavo Araújo da Silva <gustavoads1102@gmail.com>
2024-11-05 10:06:29 -0300

    Implementação da gestão de competições

---
+++ CompetiZapp/Migrations/20240919123942_Add-UseDarkMode-From-ApplicationUser-Table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompetiZapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUseDarkModeFromApplicationUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "UseDarkMode",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UseDarkMode",
+                table: "AspNetUsers");
+        }
+    }
+}
